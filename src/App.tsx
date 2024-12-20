ed4cfd3991023a5d4ac89f6af01242f4a6064abb
rMaxBarros <rmaxbarros@gmail.com>
2024-12-20 17:37:15 -0300

    Comment on last commit changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Header } from './components/Header/Header';
 
 import './styles/global.css';
@@ -9,21 +9,21 @@ function App() {
 
   // O UseEffect será disparado sempre que alguma variável do array de dependências for alterada.
   // Por Padrão, sempre o UseEffect será disparado após a montagem do componente. (ComponentDidMount)
-  useEffect( () => {
-    console.log('Executando a função do useEffect...');
+  // useEffect( () => {
+  //   console.log('Executando a função do useEffect...');
 
-    // ComponentWillUnmount é acionado no UseEffect dentro do return() passando uma função => {}, essa função é chamada Cleanup Function.
-    return () => {
-      console.log('Isso aqui vai ser executado quando o componente App for desmontado da tela');
-    };
-  }, [toggle] );
+  //   // ComponentWillUnmount é acionado no UseEffect dentro do return() passando uma função => {}, essa função é chamada Cleanup Function.
+  //   return () => {
+  //     console.log('Isso aqui vai ser executado quando o componente App for desmontado da tela');
+  //   };
+  // }, [toggle] );
 
   return (
     <>
       <Header />
       <Tasks />
 
-      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      {/* <button onClick={() => setToggle(!toggle)}>Toggle</button> */}
     </>
   );
 }
