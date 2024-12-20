fa8530b141e4fa4f0962ebf095ed323b9476cb24
rMaxBarros <rmaxbarros@gmail.com>
2024-12-20 15:26:09 -0300

    Hook State for Tasks

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import styles from './styles.module.scss';
 
 export const Tasks: React.FC = () => {
+    const [taskTitle, setTaskTitle] = useState('');
+    // ARRAY HOOKSTATE: taskTitle: o estado. setTaskTitle: função que redefine o valor do estado. É inicializado com uma string vazia nesse caso.
+
     return (
         <section className={styles.container}>
-            <form>
+            <form onSubmit={(event) => { event.preventDefault() }}>
                 <div>
                     <label htmlFor="task-title">Adicionar Tarefa</label>
-                    <input type="text" id='task-title' placeholder='Título da Tarefa' />
+                    <input
+                        value={taskTitle}
+                        onChange={(event) => setTaskTitle(event.target.value)} // Pega o valor digitado e joga dentro do estado. 
+                        type="text"
+                        id='task-title'
+                        placeholder='Título da Tarefa'
+                    />
                 </div>
 
-                <button>Adicionar Tarefa</button>
+                <button type='submit'>Adicionar Tarefa</button>
             </form>
         </section>
     )
