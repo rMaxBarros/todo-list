170079d5a76c140e2182430e382f7bdda67104d7
rMaxBarros <rmaxbarros@gmail.com>
2024-12-23 12:56:04 -0300

    useEffect updating ref with useRef

--- src/components/Concepts/Refs.tsx
+++ src/components/Concepts/Refs.tsx
@@ -1,29 +1,25 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const Refs: React.FC = () => {
-    const ref = useRef(0); // { current: 0 }
-    // Com o useRef não é disparada uma rerenderização. Isso significa que, o valor da variável pode até ser modificado como em handleClickOnButton, porém, no HTML, não será exibido o novo valor cada vez que o valor for modificado.
+    const inputRef = useRef(null); // { current: null }
 
-    //Com esse useState, ao aperta no botão de Toggle, o valor da Ref acima será atualizado no HTML pois o useState aplica uma rerenderização dos componentes.
-    const [toggle, setToggle] = useState(false);
-
-    function handleClickOnButton() {
-        ref.current = ref.current +1;
-        console.log(ref.current);
-    }
+    // Pegar a mudança do useRef com o input.
+    // Toda vez que o inputRef mudar, dá um console.log mostrando o que tem dentro dele.
+    useEffect(() => {
+        console.log(inputRef);
+    }, [inputRef]);             // Pega a referência de nulo para: { current: input }
 
     return (
         <div style={{ padding: '2rem' }}>
             <h1>useRef</h1>
 
-            <p>{`O valor da ref é ${ref.current}`}</p>
+            <br />
+            {/* Com a propriedade ref, ele pega o valor dela e joga dentro do current
+            Ele pega todas as propriedades que existem dentro do HTML Vanila, para dentro do react. */}
+            <input type="text" placeholder="Nome Completo" ref={inputRef} />
 
             <br />
-            <button onClick={handleClickOnButton}>Clique aqui</button>
-
-            <br />
-            {/* Pega o valor atual e inverte */}
-            <button onClick={() => {setToggle(!toggle)}}>Toggle</button>
+            <button>Clique aqui</button>
         </div>
     );
 }
