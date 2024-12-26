34aa671a9680ebf7b3a9eddf0a74f448189accaa
rMaxBarros <rmaxbarros@gmail.com>
2024-12-26 12:40:55 -0300

    HOOKS: useMemo - memoization

--- src/components/Concepts/Memoization.tsx
+++ src/components/Concepts/Memoization.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 interface MemoizationProps {
     financialData: {
@@ -6,18 +6,26 @@ interface MemoizationProps {
         outcomes: number[],
     }
 }
-
+// Usado em componentes com muitas propriedades/estados com cálculos que são exibidos no código.
 export const Memoization: React.FC<MemoizationProps> = ({ financialData }) => {
     // Estado que define se os valores devem ser mostrados ou não.
     const [showValues, setShowValues] = useState(true);
 
-    const totalIncomes = financialData.incomes.reduce((total, income) => {
-        return total += income
-    }, 0); // Valor inicial
+    // useMemo evita que toda vez que o componente sofra uma alteração que não seja específica da função do useMemo, ele recarregue tudo que tenha lá. 
+    // Para deixar mais claro: Ao clicar no botão de Mostrar/Ocultar valores, o reduce do icomes/outcomes não será totalmente recarregada, o valor já estará salvo e só será rerenderizado se for necessário.
+    const totalIncomes = useMemo(() => {
+        financialData.incomes.reduce((total, income) => {
+            console.log('Calculando o total de receitas...');
+            return total += income
+        }, 0); // Valor inicial
+    }, [financialData.incomes]); // Define a dependência que pode alterar o valor do totalIcomes.
 
-    const totalOutcomes = financialData.outcomes.reduce((total, outcome) => {
-        return total += outcome
-    }, 0);
+    const totalOutcomes = useMemo(() => {
+        financialData.outcomes.reduce((total, outcome) => {
+            console.log('Calculando o total de despesas...');
+            return total += outcome
+        }, 0);
+    }, [financialData.outcomes]);
 
     return (
         <div style={{ padding: "2rem" }}>
@@ -32,7 +40,7 @@ export const Memoization: React.FC<MemoizationProps> = ({ financialData }) => {
             <br />
             <br />
             <button onClick={() => setShowValues(!showValues)}>
-                { showValues ? 'Ocultar valores' : 'Mostrar valores'}
+                {showValues ? 'Ocultar valores' : 'Mostrar valores'}
             </button>
         </div>
     )
